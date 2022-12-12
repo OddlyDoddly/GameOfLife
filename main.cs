ba482e19861aedab46c757dd4ac62a63c05b1aa9
OddlyDoddly <oddlydoddly@protonmail.com>
2022-12-12 12:06:23 -0500

    updated gameloop

--- main.cs
+++ main.cs
@@ -4,23 +4,31 @@ DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
 void Main()
 {
-    int tps = 10; // Ticks Per second, determines how often game logic gets updated.
+    int tps = 20; // Ticks Per second, determines how often game logic gets updated.
+    int fps = 20;
 
-    GameWorld gameWorld = new GameWorld(new GameOfLife.system.Vector2i(120, 30));
     ConsoleScreen consoleScreen = new ConsoleScreen(
         new GameOfLife.system.Vector2i(0, 0), 
         new GameOfLife.system.Vector2i(Console.WindowWidth, Console.WindowHeight));
+    GameWorld gameWorld = new GameWorld(new GameOfLife.system.Vector2i(Console.WindowWidth, Console.WindowHeight));
 
     long lastUpdate = (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
+    long lastDraw = (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
     while (true)
     {
         long now = (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
         long deltaTime = (now - lastUpdate);
+        long deltaFrameTime = (now - lastDraw);
+
+        if (deltaFrameTime >= 1000 / fps)
+        {
+            consoleScreen.Draw(deltaTime, gameWorld);
+            lastDraw = now;
+        }
 
         // Runs an update on the gameworld's logic
         if (deltaTime >= 1000/tps)
         {
-            consoleScreen.Draw(deltaTime, gameWorld);
             gameWorld.Update(deltaTime);
             lastUpdate = now;
         }
